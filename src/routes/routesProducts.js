d649cb70e03d70f044657345feb16172f17613f1
francomostajo0 <francomostajo@gmail.com>
2024-09-27 22:47:41 -0300

    realizando modificaciones para que usuario premium pueda modificar productos

--- src/routes/routesProducts.js
+++ src/routes/routesProducts.js
@@ -7,16 +7,15 @@ import {
     updateProduct,
     deleteProduct
 } from '../controllers/product.controller.js';
-import { isAdmin } from '../middleware/auth.js';
-import { isAuthenticated } from '../middleware/auth.js';
+import { isAdmin, isAuthenticated, isPremium } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/categories', getCategories);
 router.get('/:pid', getProductById);
-router.post('/products', isAuthenticated, isAdmin, createProduct);
-router.put('/:pid', isAuthenticated, isAdmin, updateProduct);
-router.delete('/:pid', isAuthenticated, isAdmin, deleteProduct);
+router.post('/products', isAuthenticated, isAdmin, isPremium, createProduct);
+router.put('/:pid', isAuthenticated, isAdmin, isPremium, updateProduct);
+router.delete('/:pid', isAuthenticated, isAdmin, isPremium, deleteProduct);
 
 export default router;
